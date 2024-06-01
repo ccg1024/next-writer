bf0159dffe2981d4c44c40850fce33511c5e2641
ccg1024 <aimerchenyuan@163.com>
2024-06-01 11:12:45 +0800

    feat: update inline-mark toggle keymap

--- src/ui/libs/keymap.ts
+++ src/ui/libs/keymap.ts
@@ -1,4 +1,5 @@
 import { syntaxTree } from '@codemirror/language'
+import { EditorSelection } from '@codemirror/state'
 import { EditorView, KeyBinding } from '@codemirror/view'
 
 const insert = (view: EditorView, text: string, move = 0) => {
@@ -18,16 +19,74 @@ const insert = (view: EditorView, text: string, move = 0) => {
 
   return true
 }
+const inlineMarkToggle = (opt: {
+  view: EditorView
+  text: string
+  offset: number
+  markName: string
+}) => {
+  const { view, text, offset, markName } = opt
+  if (view.state.selection.main.empty)
+    return insert(view, text.repeat(2), offset)
+
+  // if select some text
+  const { from } = view.state.selection.main
+  const { node } = syntaxTree(view.state).resolve(from)
+  if (node.name === markName) {
+    // remove mark
+    view.dispatch(
+      view.state.changeByRange(range => {
+        return {
+          range: EditorSelection.range(range.from - offset, range.to - offset),
+          changes: [
+            { from: range.from - offset, to: range.from, insert: '' },
+            { from: range.to, to: range.to + offset, insert: '' }
+          ]
+        }
+      })
+    )
+    return true
+  }
+
+  // else addd mark
+  view.dispatch(
+    view.state.changeByRange(range => {
+      return {
+        range: EditorSelection.range(range.from + offset, range.to + offset),
+        changes: [
+          { from: range.from, insert: text },
+          { from: range.to, insert: text }
+        ]
+      }
+    })
+  )
+  return true
+}
 const insertBold = (view: EditorView) => {
-  return insert(view, '****', 2)
+  return inlineMarkToggle({
+    view,
+    text: '**',
+    offset: 2,
+    markName: 'StrongEmphasis'
+  })
 }
 
 const insertItalic = (view: EditorView) => {
-  return insert(view, '**', 1)
+  return inlineMarkToggle({
+    view,
+    text: '*',
+    offset: 1,
+    markName: 'Emphasis'
+  })
 }
 
 const insertCode = (view: EditorView) => {
-  return insert(view, '``', 1)
+  return inlineMarkToggle({
+    view,
+    text: '`',
+    offset: 1,
+    markName: 'InlineCode'
+  })
 }
 
 const insertBlockCode = (view: EditorView) => {
