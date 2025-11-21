61417058f35e62fbcedcfceaddeb968c7519f699
ccg1024 <aimerchenyuan@163.com>
2025-11-22 00:07:19 +0800

    feat: add pro shell

---
+++ bin/server/index.pro.js
@@ -0,0 +1,33 @@
+// exprot evironemt config
+export const NW_ENV = 'PRO';
+
+// Default config file path
+export const CONFIG_DIR_NAME = 'nwriter';
+
+export const CONFIG_JSON_NAME = 'nwriter.json';
+
+// Default log file path
+export const LOG_DIR_NAME = 'nwriter';
+
+export const LOG_NAME = 'log';
+
+// Default library name; ~/Documents/${ROOT_DIR_NAME}
+export const ROOT_DIR_NAME = 'nwriter';
+
+// Default library info; ~/Documents/${ROOT_DIR_NAME}/${ROOT_CONFIG_NAME}
+export const ROOT_CONFIG_NAME = '.nwriter.info.json';
+
+// The maximum length of the file description
+export const MAX_FILE_DESCRIPTION_LENGTH = 100;
+
+// Default config
+export const NW_CONFIG = {
+  uiFontSize: '14px',
+  editorFontSize: '14px',
+  editorFont: "'PingFang SC'",
+  codeFont: "'Hack Nerd Font'",
+  uiFont: "'Rounded Mplus 1c'",
+  focusMode: false,
+  typewriter: false,
+  hideMarks: false
+};
